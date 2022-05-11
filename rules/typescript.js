4cc2b92736e93eec7de039fc4cb57115cc0bae69
Vlado Senasi <vladimir.senasi@nalgoo.com>
2022-05-11 15:06:57 +0200

    added test

--- rules/typescript.js
+++ rules/typescript.js
@@ -23,6 +23,14 @@ module.exports = {
 		// Enforce a defaultProps definition for every prop that is not a required prop
 		// https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/require-default-props.md
 		// disable rule, we are using interfaces, types and default arguments
-		'react/require-default-props': [ 'error', { functions: 'defaultArguments' } ],
+		'react/require-default-props': ['error', {
+			forbidDefaultForRequired: true,
+
+			// will be added in next version
+			// functions: 'defaultArguments',
+
+			// will be deprecated in next version
+			ignoreFunctionalComponents: true,
+		}],
 	},
 };
---
+++ test/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	extends: [
+		'../index',
+	],
+
+	parserOptions: {
+		project: 'tsconfig.json',
+		tsconfigRootDir: __dirname,
+	}
+};
