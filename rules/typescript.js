e76d7f2305b6292fedad40ff7a4b4bd6c9a24f81
Vlado Senasi <vladimir.senasi@nalgoo.com>
2022-05-11 15:32:39 +0200

    disable react/require-default-props

--- rules/typescript.js
+++ rules/typescript.js
@@ -23,14 +23,6 @@ module.exports = {
 		// Enforce a defaultProps definition for every prop that is not a required prop
 		// https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/require-default-props.md
 		// disable rule, we are using interfaces, types and default arguments
-		'react/require-default-props': ['error', {
-			forbidDefaultForRequired: true,
-
-			// will be added in next version
-			// functions: 'defaultArguments',
-
-			// will be deprecated in next version
-			ignoreFunctionalComponents: true,
-		}],
+		'react/require-default-props': 'off',
 	},
 };
