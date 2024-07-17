563c4cd738553e4ea02c2a8acf2e3a0fa772426e
Vlado Senasi <vladimir.senasi@nalgoo.com>
2024-07-17 10:50:20 +0200

    bumped dependencies and added react-refresh plugin

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
 	extends: [
+		'./rules/react',
 		'./rules/style',
 		'./rules/typescript',
 		'./rules/storybook',
--- rules/style.js
+++ rules/style.js
@@ -3,6 +3,7 @@ const { rules: baseStyleRules } = require('eslint-config-airbnb-base/rules/style
 module.exports = {
 	plugins: [
 		'react',
+		'react-refresh',
 		'eslint-comments'
 	],
 
@@ -37,5 +38,8 @@ module.exports = {
 		// https://eslint.org/docs/rules/max-len
 		// increase limit to 120 chars (from 100)
 		'max-len': ['error', 120],
+
+		// Don't warn when a constant (string, number, boolean, templateLiteral) is exported aside one or more components.
+		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
 	},
 };
